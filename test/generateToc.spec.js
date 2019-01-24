32d80d09146b43cb87a162cc00ca1692fc020fbc
quilicicf <cquilici@restlet.com>
2019-01-24 13:40:43 +0100

    :white_check_mark: Test that formatting is idempotent

--- test/generateToc.spec.js
+++ test/generateToc.spec.js
@@ -1,18 +1,27 @@
 const { resolve: resolvePath } = require('path');
 
 const readFile = require('../lib/readFile');
-const generateToc = require('../lib/formatFromString');
+const formatFromString = require('../lib/formatFromString');
 
 const INPUT_DATA_FILE = resolvePath(__dirname, 'test_input.md');
 const OUTPUT_DATA_FILE = resolvePath(__dirname, 'test_output.md');
 
 describe('Generate ToC', () => {
-  test('It should generate a ToC', async () => {
+  test('It should format', async () => {
     const inputMarkdownString = await readFile(INPUT_DATA_FILE);
     const outputMarkdownString = await readFile(OUTPUT_DATA_FILE);
 
-    const markdownWithToc = await generateToc(inputMarkdownString);
+    const markdownWithToc = await formatFromString(inputMarkdownString);
 
     expect(markdownWithToc).toBe(outputMarkdownString);
+  });
+
+  test('It should be idempotent', async () => {
+    const inputMarkdownString = await readFile(INPUT_DATA_FILE);
+
+    const markdownFormattedOnce = await formatFromString(inputMarkdownString);
+    const markdownFormattedTwice = await formatFromString(markdownFormattedOnce);
+
+    expect(markdownFormattedTwice).toBe(markdownFormattedOnce);
   });
 });
