edb8065a234ae1679f27c9a73a541f6c2e8c4f32
quilicicf <cquilici@restlet.com>
2019-01-26 17:49:29 +0100

    :white_check_mark: Test formatting from file

--- test/generateToc.spec.js
+++ test/generateToc.spec.js
@@ -1,17 +1,25 @@
 const { resolve: resolvePath } = require('path');
 
 const readFile = require('../lib/readFile');
-const formatFromString = require('../lib/formatFromString');
+const { formatFromFile, formatFromString } = require('../index');
 
 const INPUT_DATA_FILE = resolvePath(__dirname, 'test_input.md');
 const OUTPUT_DATA_FILE = resolvePath(__dirname, 'test_output.md');
 
-describe('Generate ToC', () => {
-  test('It should format', async () => {
+describe('Format', () => {
+  test('It should format from string', async () => {
     const inputMarkdownString = await readFile(INPUT_DATA_FILE);
     const outputMarkdownString = await readFile(OUTPUT_DATA_FILE);
 
     const markdownWithToc = await formatFromString(inputMarkdownString);
+
+    expect(markdownWithToc).toBe(outputMarkdownString);
+  });
+
+  test('It should format from file', async () => {
+    const outputMarkdownString = await readFile(OUTPUT_DATA_FILE);
+
+    const markdownWithToc = await formatFromFile(INPUT_DATA_FILE);
 
     expect(markdownWithToc).toBe(outputMarkdownString);
   });
