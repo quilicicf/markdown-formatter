b484385ea9b9484f705a0e8098251a48f94baa9a
quilicicf <quilicicf@gmail.com>
2021-08-23 11:03:08 +0200

    :bug: Another attempt at fixing TS type declarations

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,12 +1,17 @@
 import { VFile } from 'vfile';
-import { RemarkStringifyOptions } from 'remark-stringify';
+import { Options as RemarkStringifyOptions } from 'remark-stringify';
 
-export interface MarkdownFormatterOptions {
+interface MarkdownFormatterOptions {
   watermark?: // Add a watermark with a link to this repository's home page
     'none' // No watermark
     | 'top' // Watermark inserted at the top of the document
     | 'toc'; // Watermark added to the ToC's end tag
 }
+
+export {
+  MarkdownFormatterOptions,
+  RemarkStringifyOptions,
+};
 
 export function formatFromString (
   sourceMarkdownString: string,
